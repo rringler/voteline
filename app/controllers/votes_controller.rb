cef7dbd96287f0c4ec043639fd3dbcff27535deb
Ryan Ringler <rringler@gmail.com>
2014-05-25 17:29:37 -0700

    Updates before_filter typo, adds javascript as a supported typo to the #create action, and updates the permitted params

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
-  befole_filter :current_user?, only: [:edit, :update, :destroy]
+  before_filter :current_user?, only: [:edit, :update, :destroy]
 
   def new
     @vote = Vote.new
@@ -10,11 +10,13 @@ class VotesController < ApplicationController
     @vote = Vote.new(vote_params)
     @vote.user_id = current_user.id
 
-    if @vote.save
-      redirect_to poll_vote_show_path(@vote),
-                  flash: { success: "Created casted a vote!" }
-    else
-      render 'new'
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_to poll_path(@vote.poll), success: "Voted!" }
+        format.js   { render 'polls/show', locals: { poll: @vote.poll.decorate } }
+      else
+        format.html { render 'new' }
+      end
     end
   end
 
@@ -28,13 +30,8 @@ class VotesController < ApplicationController
   private
 
   def vote_params
-    params.require(:vote).permit(:question,
-                                 :vote_min,
-                                 :vote_max,
-                                 :start_date,
-                                 :start_time,
-                                 :finish_date,
-                                 :finish_time)
+    params.require(:vote).permit(:poll_id,
+                                 :value)
   end
 
   def current_user?

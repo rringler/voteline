8e34310c7272fb45c272405a627ce80898065e3f
Ryan Ringler <rringler@gmail.com>
2014-05-25 17:26:11 -0700

    Updates Polls to add recent scope and verify presence of start & finish datetimes

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,5 +2,12 @@ class Poll < ActiveRecord::Base
   belongs_to :user
   has_many   :votes
 
-  attr_accessor :start_date, :finish_date
+  scope :recent_start, ->(limit = 5) { order('start DESC').limit(limit) }
+
+  validates :start,  presence: true
+  validates :finish, presence: true
+
+  def range
+    (vote_min..vote_max)
+  end
 end
